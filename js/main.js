baf890eb7cf70b25fdb113295a123a62146d980b
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-14 12:01:34 -0700

    Finish adding validation of matching passwords

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+const password = document.getElementById('password')
+const confirmedPassword = document.getElementById('confirmed_password')
+const passText = document.querySelector('.invalid_pass')
+
+confirmedPassword.addEventListener('keyup', matchPass)
+
+function matchPass(e){
+    if(password.value !== confirmedPassword.value){
+        // alert('sheeez')
+        password.classList.toggle('invalid')
+        confirmedPassword.classList.toggle('invalid')
+        passText.classList.toggle('hidden')
+    }
+}
